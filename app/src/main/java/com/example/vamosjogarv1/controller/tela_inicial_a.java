50cd263baa4f35cda1d15091d8c48345e9ee3bfe
danielabina <danibinina@gmail.com>
2020-08-15 13:56:18 -0300

    08/08 - inicio back on

--- app/src/main/java/com/example/vamosjogarv1/controller/tela_inicial_a.java
+++ app/src/main/java/com/example/vamosjogarv1/controller/tela_inicial_a.java
@@ -2,7 +2,10 @@ package com.example.vamosjogarv1.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.vamosjogarv1.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class tela_inicial_a extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class tela_inicial_a extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial_a);
+    }
+
+    public void MeusEvento(View view) {
+        Intent it = new Intent(tela_inicial_a.this, tela_menu_eventos.class);
+        startActivity(it);
+    }
+    public void BuscarEvento(View view) {
+        Intent it = new Intent(tela_inicial_a.this, tela_buscar_evento.class);
+        startActivity(it);
+    }
+
+    public void CadastrarEvento(View view) {
+        Intent it = new Intent(tela_inicial_a.this, tela_cadastrar_evento.class);
+        startActivity(it);
     }
 }
